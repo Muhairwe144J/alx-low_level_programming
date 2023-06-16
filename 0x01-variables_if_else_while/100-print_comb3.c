48772ec64e76aa1b3d44cd4cabf280cface9529c
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-06-16 21:38:56 +0300

    variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,5 +6,24 @@
 */
 int main(void)
 {
+	int n, m;
+
+	for (n = 48; n <56; n++)
+	{
+		for (m = n + 1; m <= 57; m++)
+		{
+			if (n != m)
+			{
+			putchar(n);
+			putchar(m);
+			if (n == 56 || m == 57)
+				continue;
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+}
+	putchar('\n');
 	return (0);
 }
