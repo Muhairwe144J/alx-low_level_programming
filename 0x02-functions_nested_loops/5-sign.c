f51a3c2b8a8922ef64174407793769fc88ad4166
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-06-21 06:40:31 +0300

    nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - Prints the sign of a number
  * @c: The number to be checked
- * Return: 1 for positive bum, -1 for negative num or 0 for anything else
+ * Return: 1 for positive num, 0 for zero or -1 for anything else
  */
 
 int print_sign(int n)
@@ -14,14 +14,14 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
+	else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
 	}
 }
