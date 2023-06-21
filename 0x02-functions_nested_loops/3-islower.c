2d7d65476c84bba53d647ec11dff675641ceecba
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-06-21 07:07:52 +0300

    nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,18 @@
  *Return: 1 for lowercase character or 0 for anything else
  */
 
-int _islower(int c);
+int _islower(int c)
 
 {
 
-	if (c >= 97 && c <= 122)3-islower.c
+	if (c >= 97 && c <= 122)
 	{
 
 		return (1);
 	}
+	else
+	{
 	return (0);
+	}
+	_putchar('\n');
 }
