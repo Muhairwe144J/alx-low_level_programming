92b29321be26f291cc7a241addeb3c46037290fe
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-06-22 23:51:33 +0300

    nested loops 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,24 +4,32 @@
 /**
  * fizzbuzz - Prints the numbers from 1 to 100 with some exceptions.
  */
+void fizzbuzz(void)
+{
+int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+
+printf("\n");
+}
+
+/**
+ * main - Entry point of the program
+ * Return: 0 on success
+ */
 int main(void)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else
-		{
-			printf("%d ", i);
-		}
-	}
-
-	printf("\n");
-	return (0);
+fizzbuzz();
+return (0);
 }
+
