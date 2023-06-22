71682f8366dd9bc36d24938925de5c9213cd4b96
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-06-22 23:37:24 +0300

    nested loops 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,12 @@ void more_numbers(void)
 {
 	int i, c;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-			if (c >= 10)
-				_putchar('1');
+			if (c > 9)
+				_putchar((c / 10) + '0');
 
 			_putchar((c % 10) + '0');
 		}
