3c5c00c46a3e45373606c5a86ed6ad697b0247e0
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-01 01:47:39 +0300

    more strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,55 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * infinite_add - function that adds two numbers
- * @n1: one of the two numbers
- * @n2: one of the two numbers
- * @r: buffer that the function will use to store the result
- * @size_r: the buffer size
+ * infinite_add - Adds two numbers
+ * @n1: The first number as a string
+ * @n2: The second number as a string
+ * @r: The buffer to store the result
+ * @size_r: The size of the buffer
  *
- * Return: always (0)
+ * Return: Pointer to the result (r), or 0 if result cannot be stored
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0;
+	int i, j, k;
+	int len_n1 = 0, len_n2 = 0, len_sum = 0;
+	int carry = 0, digit_sum = 0;
 
-while (*(n1 + i) != '\0')
-i++;
-while (*(n2 + j) != '\0')
-j++;
-i--;
-j--;
-if (j >= size_r || i >= size_r)
-	return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
-{
-	if (i < 0)
-		val1 = 0;
-	else
-	val1 = *(n1 + i) -'0';
-	if (j < 0)
-	val2 = 0;
-	else
-	val2 = *(n2 + j) -'0';
-	temp_tot = val1 + val2 + overflow;
-	if (temp_tot >= 10)
-		overflow = 1;
-	else
-		overflow = 0;
-	if (digits >= (size_r - 1))
+	while (n1[len_n1] != '\0')
+		len_n1++;
+	while (n2[len_n2] != '\0')
+		len_n2++;
+
+	if (len_n1 > size_r || len_n2 > size_r)
 		return (0);
-	*(r + digits) = (temp_tot % 10) + '0';
-	digits++;
-	j--;
-	i--;
-}
-if (digits == size_r)
-	return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
+
+	len_sum = (len_n1 > len_n2) ? len_n1 : len_n2;
+	r[len_sum] = '\0';
+
+	for (i = len_n1 - 1, j = len_n2 - 1, k = len_sum - 1; k >= 0; i--, j--, k--)
+	{
+		digit_sum = carry;
+		if (i >= 0)
+			digit_sum += n1[i] - '0';
+		if (j >= 0)
+			digit_sum += n2[j] - '0';
+
+		carry = digit_sum / 10;
+		r[k] = (digit_sum % 10) + '0';
 	}
+
+	if (carry)
+	{
+		if (len_sum + 1 > size_r)
+			return (0);
+
+		for (i = len_sum; i >= 0; i--)
+			r[i + 1] = r[i];
+
+		r[0] = carry + '0';
+	}
+
+	return (r);
+}
+
