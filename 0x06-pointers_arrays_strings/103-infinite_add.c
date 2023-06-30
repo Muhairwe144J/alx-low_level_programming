9aa221de530dfcccedbedba9366bb1a1cd52dda6
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-01 02:23:28 +0300

    more strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,64 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * infinite_add - Adds two numbers
- * @n1: The first number
- * @n2: The second number
+ * @n1: The first number as a string
+ * @n2: The second number as a string
  * @r: The buffer to store the result
  * @size_r: The size of the buffer
  *
- * Return: A pointer to the result
+ * Return: Pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int len1 = 0, len2 = 0, carry = 0, sum = 0;
-int i, j, k;
+	int len1, len2, i, j, carry, sum;
 
-/* Calculate the lengths of n1 and n2 */
-while (n1[len1] != '\0')
-len1++;
-while (n2[len2] != '\0')
-len2++;
+	len1 = strlen(n1);
+	len2 = strlen(n2);
 
-/* Check if the result can be stored in r */
-if (len1 + len2 + 1 > size_r)
-return (0);
+	if (len1 > size_r || len2 > size_r)
+		return (0);
 
-i = len1 - 1;
-j = len2 - 1;
-k = size_r - 1;
+	carry = 0;
+	i = len1 - 1;
+	j = len2 - 1;
+	size_r--;
+	r[size_r] = '\0';
 
-/* Add digits from right to left */
-while (i >= 0 || j >= 0 || carry)
-{
-sum = carry;
-if (i >= 0)
-sum += n1[i--] - '0';
-if (j >= 0)
-sum += n2[j--] - '0';
+	while (i >= 0 || j >= 0)
+	{
+		if (i >= 0)
+			sum = carry + (n1[i] - '0');
+		else
+			sum = carry;
 
-carry = sum / 10;
-r[k--] = (sum % 10) + '0';
-}
+		if (j >= 0)
+			sum += (n2[j] - '0');
 
-/* Shift the result to the beginning of the buffer if necessary */
-if (k >= 0)
-{
-i = 0;
-while (r[k] != '\0')
-r[i++] = r[k++];
-r[i] = '\0';
-}
+		carry = sum / 10;
 
-return (r);
+		if (size_r > 0)
+		{
+			r[--size_r] = (sum % 10) + '0';
+			i--;
+			j--;
+		}
+		else
+			return (0);
+	}
+
+	if (carry == 1 && size_r > 0)
+	{
+		r[--size_r] = carry + '0';
+		return (&r[size_r]);
+	}
+	else if (carry == 0)
+	{
+		return (&r[size_r]);
+	}
+	else
+	{
+		return (0);
+	}
 }
