3d6b3a0bb1a9af67d39a04e8108de47c4d438b5d
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-01 02:18:08 +0300

    more strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * infinite_add - Adds two numbers
- * @n1: The first number as a string
- * @n2: The second number as a string
+ * @n1: The first number
+ * @n2: The second number
  * @r: The buffer to store the result
  * @size_r: The size of the buffer
  *
- * Return: Pointer to the result (r), or 0 if result cannot be stored
+ * Return: A pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k;
-	int len_n1 = 0, len_n2 = 0, len_sum = 0;
-	int carry = 0, digit_sum = 0;
+int len1 = 0, len2 = 0, carry = 0, sum = 0;
+int i, j, k;
 
-	while (n1[len_n1] != '\0')
-		len_n1++;
-	while (n2[len_n2] != '\0')
-		len_n2++;
+/* Calculate the lengths of n1 and n2 */
+while (n1[len1] != '\0')
+len1++;
+while (n2[len2] != '\0')
+len2++;
 
-	if (len_n1 > size_r || len_n2 > size_r)
-		return (0);
+/* Check if the result can be stored in r */
+if (len1 + len2 + 1 > size_r)
+return (0);
 
-	len_sum = (len_n1 > len_n2) ? len_n1 : len_n2;
-	r[len_sum] = '\0';
+i = len1 - 1;
+j = len2 - 1;
+k = size_r - 1;
 
-	for (i = len_n1 - 1, j = len_n2 - 1, k = len_sum - 1; k >= 0; i--, j--, k--)
-	{
-		digit_sum = carry;
-		if (i >= 0)
-			digit_sum += n1[i] - '0';
-		if (j >= 0)
-			digit_sum += n2[j] - '0';
+/* Add digits from right to left */
+while (i >= 0 || j >= 0 || carry)
+{
+sum = carry;
+if (i >= 0)
+sum += n1[i--] - '0';
+if (j >= 0)
+sum += n2[j--] - '0';
 
-		carry = digit_sum / 10;
-		r[k] = (digit_sum % 10) + '0';
-	}
-
-	if (carry)
-	{
-		if (len_sum + 1 > size_r)
-			return (0);
-
-		for (i = len_sum; i >= 0; i--)
-			r[i + 1] = r[i];
-
-		r[0] = carry + '0';
-	}
-
-	return (r);
+carry = sum / 10;
+r[k--] = (sum % 10) + '0';
 }
 
+/* Shift the result to the beginning of the buffer if necessary */
+if (k >= 0)
+{
+i = 0;
+while (r[k] != '\0')
+r[i++] = r[k++];
+r[i] = '\0';
+}
+
+return (r);
+}
