f8022ce0fc9a38f612bcc6e39af5b36101a117db
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-06 01:32:01 +0300

    recursions

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,46 +1,30 @@
 #include "main.h"
 
 /**
- * _sqrt_helper - Finds the square root of a number using binary search.
+ * actual_sqrt_recursion - Recurses to find the natural square root of a number
  * @n: The number to find the square root of.
- * @min: The lower bound of the search range.
- * @max: The upper bound of the search range.
+ * @i: Iterator.
  *
- * Return: The square root of n,
- * or -1 if it doesn't have a natural square root.
+ * Return: The square root of n.
  */
-int _sqrt_helper(int n, int min, int max)
+int actual_sqrt_recursion(int n, int i)
 {
-int mid;
-
-if (min > max) /* Base case - square root not found */
+if (i * i > n)
 return (-1);
-
-mid = (min + max) / 2; /* Find the middle element */
-
-if (mid *mid == n) /* Base case - square root found */
-return (mid);
-
-if (mid *mid > n) /* Search in the lower half */
-return (_sqrt_helper(n, min, mid - 1));
-
-  /* Search in the upper half */
-return (_sqrt_helper(n, mid + 1, max));
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
 }
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to find the square root of.
  *
- * Return: The square root of n,
- * or -1 if it doesn't have a natural square root.
+ * Return: The square root of n.
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0) /* Base case - invalid input */
+if (n < 0)
 return (-1);
-
-return (_sqrt_helper(n, 0, n));
-/* Start the search with the range [0, n/2] */
+return (actual_sqrt_recursion(n, 0));
 }
-
