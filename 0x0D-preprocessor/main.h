e6e43e1d0d7b39197875730e9cf926c3138e7aff
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-17 21:08:21 +0300

    preprocessor

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Task 0: Object-like Macro */
+#define SIZE 1024
+
+/* Task 1: Pi */
+#define PI 3.14159265359
+
+/* Task 3: Function-like Macro */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/* Task 4: SUM */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* MAIN_H */
