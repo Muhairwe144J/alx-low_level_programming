573585732346fc923531b2850674c3260694333f
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-17 20:31:52 +0300

    structure

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Include necessary header files */
+#include "dog.h"  // Include the dog.h header file for struct and function declarations
+
+/* Function prototypes */
+int main(void);  // Declaration of the main function
+
+#endif /* MAIN_H */
