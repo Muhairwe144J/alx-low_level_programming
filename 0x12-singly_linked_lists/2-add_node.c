c00825ae0e95432af8ed070a78610b3c1b67d588
Muhairwe144J <mnasasirashafik144j@gmail.com>
2023-07-27 22:14:26 +0300

    singly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,13 +18,13 @@ len++;
 
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-return NULL;
+return (NULL);
 
 new_node->str = strdup(str);
 if (new_node->str == NULL) /* Check if strdup failed */
 {
 free(new_node);
-return NULL;
+return (NULL);
 }
 
 new_node->len = len;
@@ -32,5 +32,5 @@ new_node->next = *head;
 
 *head = new_node;
 
-return new_node;
+return (new_node);
 }
